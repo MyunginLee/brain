60a7abbdb7f9ee2c77682821b63c43a9abe4fc2e
Myungin Lee <benedic.lee@gmail.com>
2023-03-29 22:46:44 -0700

    brain for demo

--- _object.cpp
+++ _object.cpp
@@ -22,7 +22,7 @@
 #include "al/graphics/al_Shapes.hpp"
 #include "al/scene/al_PolySynth.hpp"
 #include "al/scene/al_SynthSequencer.hpp"
-// #include "al/ui/al_ControlGUI.hpp"
+#include "al/ui/al_ControlGUI.hpp"
 #include "al/ui/al_Parameter.hpp"
 #include "al/math/al_Random.hpp"
 #include "al/sound/al_Reverb.hpp"
@@ -31,63 +31,56 @@
 #include <cstdint>
 #include <vector>
 #include "soundSynth.cpp"
-#include "al/app/al_GUIDomain.hpp"
-#include "al/sphere/al_SphereUtils.hpp"
 
 using namespace gam;
 using namespace al;
 using namespace std;
 #define FFT_SIZE 4048
-static bool fullscreen = true;
 
 class MyApp : public App
 {
 public:
-  SynthGUIManager<FMWT> synthManager{"synth"};
+  SynthGUIManager<FMWT> synthManager{"synth6"};
   int midiNote;
   FMWT fm;
   Mesh mSpectrogram;
   vector<float> spectrum;
-  // bool showGUI = true;
-  // bool half_draw = false;
-  // bool diff_color = false;
-  // bool navi = false;
-  bool showSpectro = false;
+  bool showGUI = true;
+  bool half_draw = false;
+  bool showSpectro = true;
+  bool navi = false;
   gam::STFT stft = gam::STFT(FFT_SIZE, FFT_SIZE / 4, 0, gam::HANN, gam::MAG_FREQ);
   Scene *ascene1{nullptr};
   Scene *ascene2{nullptr};
   Vec3f scene_min, scene_max, scene_center;
   Vec3f scene_min_half, scene_max_half, scene_center_half;
-  vector<Mesh> mMesh; 
+  vector<Mesh> mMesh;
   vector<Mesh> mMesh_half;
-  Mesh center_of_part_mesh;
-
   Texture transparent_tex;
-  double time;
-  Light light;
-  // ControlGUI *gui;
 
-  ParameterBool showGUI{"showGUI", "", 1.0};
-  ParameterBool half_draw{"Show Half Brain", "", 0.0};
-  ParameterBool diff_color{"Differentiate Parts (color)", "", 0.0};
-  ParameterBool navi{"Navigate", "", 1.0};
-  ParameterInt upper_draw{"Draw Control", "",204,"", 1, 204};
-
-  void PlatformSetupSize()
-  {
-    int total_width, total_height;
-    al::sphere::getFullscreenDimension(&total_width, &total_height);
-    std::cout << total_width << ", " << total_height << std::endl;
-    dimensions(0, 0, total_width, total_height);
+  vector<Mesh> importOBJ(std::string fileName, vector<Mesh> mMesh){
+    Scene *ascene{nullptr};
+    ascene = Scene::import(fileName);
+    if (!ascene)
+    {
+      printf("error reading %s\n", fileName.c_str());
+      // return;
+    }
+    ascene->getBounds(scene_min, scene_max);
+    scene_center = (scene_min + scene_max) / 2.f;
+    // ascene->print();
+    mMesh.resize(ascene->meshes());
+    for (int i = 0; i < ascene->meshes(); i += 1)
+    {
+      ascene->mesh(i, mMesh[i]);
+    }
+    return mMesh;
   }
+
   virtual void onInit() override
   {
-    if (al_get_hostname() == "moxi" || fullscreen)
-    {
-      PlatformSetupSize();
-    }    
-    nav().pos(0, 3, 30);
-    lens().near(0.1).far(1000).fovy(60);
+    nav().pos(0, 30, 150);
+    lens().near(0.1).far(1000).fovy(90);
 
     // Generate a texture with an alpha channel for transparency
     transparent_tex.create2D(256, 256, Texture::RGBA8);
@@ -95,44 +88,12 @@ public:
     int Ny = transparent_tex.height();
     vector<Colori> pix;
     pix.resize(Nx * Ny);
+    std::string fileName1 = "../obj/brain.obj";
+    std::string fileName2 = "../obj/brain_half.obj";
 
-    // Import .obj file for the mesh
-    // std::string fileName = "../obj/ducky.obj";
-    std::string fileName = "../obj/brain.obj";
-    ascene1 = Scene::import(fileName);
-    if (!ascene1)
-    {
-      printf("error reading %s\n", fileName.c_str());
-      return;
-    }
-    ascene1->getBounds(scene_min, scene_max);
-    scene_center = (scene_min + scene_max) / 2.f;
-    // ascene->print();
-    mMesh.resize(ascene1->meshes());
-    for (int i = 0; i < ascene1->meshes(); i += 1)
-    {
-      ascene1->mesh(i, mMesh[i]);
-    }
-    transparent_tex.submit(mMesh.data());
-
-    // Half
-    fileName = "../obj/brain_half.obj";
-    ascene2 = Scene::import(fileName);
-    if (!ascene2)
-    {
-      printf("error reading %s\n", fileName.c_str());
-      return;
-    }
-    ascene2->getBounds(scene_min_half, scene_max_half);
-    scene_center_half = (scene_min_half + scene_max_half) / 2.f;
-    // ascene->print();
-    mMesh_half.resize(ascene2->meshes());
-    for (int i = 0; i < ascene2->meshes(); i += 1)
-    {
-      ascene2->mesh(i, mMesh_half[i]);
-    }
-    transparent_tex.submit(mMesh.data());
-
+    importOBJ(fileName1, mMesh);
+    importOBJ(fileName2, mMesh_half);
+    
     // Declare the size of the spectrum
     spectrum.resize(FFT_SIZE / 2 + 1);
     imguiInit();
@@ -148,14 +109,6 @@ public:
     // Play example sequence. Comment this line to start from scratch
     //    synthManager.synthSequencer().playSequence("synth2.synthSequence");
     synthManager.synthRecorder().verbose(true);
-    // Initialize GUI and Parameter callbacks
-    // if (isPrimary()) 
-    {
-      // auto guiDomain = GUIDomain::enableGUI(defaultWindowDomain());
-      // gui = &guiDomain->newGUI();
-      // *gui << showGUI << half_draw << diff_color << navi;
-      // *gui << sequencer << recorder;
-    }
   }
 
   void onSound(AudioIOData &io) override
@@ -186,10 +139,6 @@ public:
     imguiEndFrame();
     // Map table number to table in memory
     fm.mtable = int(synthManager.voice()->getInternalParameterValue("table"));
-    time += dt;
-    Vec3f point_you_want_to_see = Vec3f(0,0,0);
-    light.pos(nav().pos().x, nav().pos().y, nav().pos().z);
-    light.dir(point_you_want_to_see.x, point_you_want_to_see.y, point_you_want_to_see.z);
   }
 
   void onDraw(Graphics &g) override
@@ -215,61 +164,42 @@ public:
     }
     gl::depthTesting(true);
     g.lighting(true);
+    float tmp = scene_max[0] - scene_min[0];
+    // // half brain draw
+    if (half_draw)
     {
       g.pushMatrix();
-      float tmp = std::max(scene_max[1] - scene_min[1], tmp);
+      tmp = scene_max_half[0] - scene_max_half[0];
+      tmp = std::max(scene_max_half[1] - scene_max_half[1], tmp);
+      tmp = std::max(scene_max_half[2] - scene_max_half[2], tmp);
+      tmp = 2.f / tmp;
+      g.scale(tmp);
+      g.scale(100);
+      g.color(RGB(242 / 255., 174 / 255., 177 / 255.));
+      for (auto &m : mMesh_half)
+      {
+        g.draw(m);
+      }
+      // g.blending(false);
+      // g.blendTrans();
+
+      g.popMatrix();
+    }
+    else    // Full brain draw
+    {
+      g.pushMatrix();
+      tmp = std::max(scene_max[1] - scene_min[1], tmp);
       tmp = std::max(scene_max[2] - scene_min[2], tmp);
       tmp = 2.f / tmp;
       g.scale(tmp);
-      g.scale(10);
+      g.scale(100);
       g.meshColor();
-      float color_index;
+      g.color(RGB(242 / 255., 174 / 255., 177 / 255.));
       // g.texture();
       // transparent_tex.bind();
-      if (half_draw)
+      for (auto &m : mMesh)
       {
-        g.translate(0,0.011,0);
-        for (auto &m : mMesh_half)
-        // half 137 parts of brain
-        {
-          if(color_index < upper_draw-67)
-          {   
-            color_index +=1;
-            g.translate( 0.000001 * sin(color_index* time*0.1), 0.000001 * sin(time*0.2), 0.000001 * cos(color_index* time*0.8) );
-            // g.color(RGB( (color_index+200) / 255., 174 / 255., 177 / 255.)); 
-            if(diff_color){
-              g.color(HSV( (color_index) / 204., (sin(time*0.05*color_index)*0.1*color_index + 157) / 255., 177 / 255.)); 
-            } else{
-              g.color(RGB( (0.1*color_index+230) / 255., 174 / 255., 177 / 255.)); 
-            }
-            // g.color(RGB(242 / 255., 174 / 255., 177 / 255.));
-            g.draw(m);
-          }
-        }
-      }
-      else
-      {
-        g.pushMatrix();      
-        for (auto &m : mMesh)
-        // 204 parts of brain
-        {
-          if(color_index < upper_draw)
-          {       
-            color_index +=1;
-          g.translate( 0.000001 * sin(color_index* time*0.1), 0.000001 * sin(time*0.2), 0.000001 * cos(color_index* time*0.8) );
-            // g.color(RGB( (color_index+200) / 255., 174 / 255., 177 / 255.)); 
-            if(diff_color){
-              g.color(HSV( (color_index) / 204., (sin(time*0.05*color_index)*0.1*color_index + 157) / 255., 177 / 255.)); 
-            } else{
-              g.color(RGB( (0.1*color_index+230) / 255., 177/ 255., 177 / 255.)); 
-            }
-            // g.color(RGB(242 / 255., 174 / 255., 177 / 255.));
-            g.draw(m);
-            g.draw(center_of_part_mesh);
-          }
-        }
-        g.popMatrix();
-
+        g.draw(m);
       }
       g.blending(true);
       g.blendTrans();
@@ -325,21 +255,6 @@ public:
       break;
     case '-':
       half_draw = !half_draw;
-      break;
-    case '0':
-      diff_color = !diff_color;
-      break;
-    case '7':
-      upper_draw = 204;
-    case '8':
-      upper_draw = upper_draw-1;
-      if(upper_draw<2)
-        upper_draw = 1;
-      break;
-    case '9':
-      upper_draw = upper_draw+1;
-      if(upper_draw>204)
-        upper_draw = 204;
       break;
     }
     return true;
---
+++ soundSynth.cpp
@@ -0,0 +1,297 @@
+using namespace gam;
+using namespace al;
+using namespace std;
+#define FFT_SIZE 4048
+// tables for oscillator
+gam::ArrayPow2<float> tbSaw(2048), tbSqr(2048), tbImp(2048), tbSin(2048),
+    tbPls(2048), tb__1(2048), tb__2(2048), tb__3(2048), tb__4(2048);
+
+class FMWT : public SynthVoice
+{
+public:
+  // Unit generators
+  gam::Pan<> mPan;
+  gam::ADSR<> mAmpEnv;
+  gam::ADSR<> mModEnv;
+  gam::EnvFollow<> mEnvFollow;
+  gam::ADSR<> mVibEnv;
+  Reverb<float> reverb;
+  gam::Biquad<> mFilter{};
+
+  gam::Sine<> mod, mVib; // carrier, modulator sine oscillators
+  gam::Osc<> car;
+  double a = 0;
+  double b = 0;
+  double timepose = 10;
+  Vec3f init_pose, init_rot;
+  float init_rot_quant;
+  // Additional members
+  float mVibFrq;
+  float mVibDepth;
+  float mVibRise;
+  int mtable;
+  static const int numb_waveform = 9;
+  Mesh mMesh[numb_waveform];
+  bool wireframe = false;
+  bool vertexLight = false;
+
+  void init() override
+  {
+    reverb.bandwidth(0.6f); // Low-pass amount on input, in [0,1]
+    reverb.damping(0.5f);   // High-frequency damping, in [0,1]
+    reverb.decay(0.6f);     // Tail decay factor, in [0,1]
+
+    // Diffusion amounts
+    // Values near 0.7 are recommended. Moving further away from 0.7 will lead
+    // to more distinct echoes.
+    reverb.diffusion(0.76, 0.666, 0.707, 0.571);
+    mModEnv.levels(0, 1, 1, 0);
+    mVibEnv.levels(0, 1, 1, 0);
+    mAmpEnv.sustainPoint(2);
+    mFilter.type(gam::FilterType(0));
+
+    createInternalTriggerParameter("frequency", 440, 10, 4000.0);
+    createInternalTriggerParameter("amplitude", 0.1, 0.0, 1.0);
+    createInternalTriggerParameter("attackTime", 0.1, 0.01, 3.0);
+    createInternalTriggerParameter("releaseTime", 5., 0.1, 10.0);
+    createInternalTriggerParameter("sustain", 0.65, 0.1, 1.0);
+    // FM index
+    createInternalTriggerParameter("idx1", 0.01, 0.0, 10.0);
+    createInternalTriggerParameter("idx2", 7, 0.0, 10.0);
+    createInternalTriggerParameter("idx3", 5, 0.0, 10.0);
+
+    createInternalTriggerParameter("carMul", 1.7, 0.0, 20.0);
+    createInternalTriggerParameter("modMul", 1.30, 0.0, 20.0);
+
+    createInternalTriggerParameter("vibRate1", 0.01, 0.0, 10.0);
+    createInternalTriggerParameter("vibRate2", 0.5, 0.0, 10.0);
+    createInternalTriggerParameter("vibRise", 0, 0.0, 10.0);
+    createInternalTriggerParameter("vibDepth", 0, 0.0, 10.0);
+
+    createInternalTriggerParameter("pan", 0.0, -1.0, 1.0);
+    createInternalTriggerParameter("table", 1, 0, 8);
+    createInternalTriggerParameter("reverb", 0.95, 0., 1);
+    // 	LOW_PASS,	HIGH_PASS
+    createInternalTriggerParameter("filterType", 0, 0, 2); 
+    createInternalTriggerParameter("filterFrequency", 1200, 5, 5000);
+    createInternalTriggerParameter("filterResonance", 1.0, 0.1f, 20);
+
+    // Table & Visual meshes
+    // Now We have the mesh according to the waveform
+    gam::addSinesPow<1>(tbSaw, 9, 1);
+    addCone(mMesh[0],1, Vec3f(0,0,5), 40, 1); //tbSaw
+
+    gam::addSinesPow<1>(tbSqr, 9, 2);
+    addCube(mMesh[1]);  // tbSquare
+
+    gam::addSinesPow<0>(tbImp, 9, 1);
+    addPrism(mMesh[2],1,1,1,100); // tbImp
+
+    gam::addSine(tbSin);
+    addSphere(mMesh[3], 0.3, 16, 100); // tbSin
+
+// About: addSines (dst, amps, cycs, numh)
+// \param[out] dst		destination array
+// \param[in] amps		harmonic amplitudes of series, size must be numh - A[]
+// \param[in] cycs		harmonic numbers of series, size must be numh - C[]
+// \param[in] numh		total number of harmonics
+    float scaler = 0.15;
+    float hscaler = 1;
+
+    { //tbPls
+      float A[] = {1, 1, 1, 1, 0.7, 0.5, 0.3, 0.1};
+      gam::addSines(tbPls, A, 8); 
+      addWireBox(mMesh[4],2);    // tbPls
+    }
+    { // tb__1 
+      float A[] = {1, 0.4, 0.65, 0.3, 0.18, 0.08, 0, 0};
+      float C[] = {1, 4, 7, 11, 15, 18, 0, 0 };
+      gam::addSines(tb__1, A, C, 6);
+      for (int i = 0; i < 7; i++){
+        addWireBox(mMesh[5], scaler * A[i]*C[i], scaler * A[i+1]*C[i+1], 1 + 0.3*i);
+
+        // addSphere(mMesh[5],scaler * A[i], 16, 30); // tb__1
+      }
+    }
+    { // inharmonic partials
+      float A[] = {0.5, 0.8, 0.7, 1, 0.3, 0.4, 0.2, 0.12};
+      float C[] = {3, 4, 7, 8, 11, 12, 15, 16}; 
+      gam::addSines(tb__2, A, C, 8); // tb__2
+      for (int i = 0; i < 7; i++){
+        addWireBox(mMesh[6], scaler * A[i]*C[i], scaler * A[i+1]*C[i+1], 1 + 0.3*i);
+      }
+    }
+    { // inharmonic partials
+      float A[] = {1, 0.7, 0.45, 0.3, 0.15, 0.08, 0 , 0};
+      float C[] = {10, 27, 54, 81, 108, 135, 0, 0};
+      gam::addSines(tb__3, A, C, 6); // tb__3
+      for (int i = 0; i < 7; i++){
+        addWireBox(mMesh[7], scaler * A[i]*C[i], scaler * A[i+1]*C[i+1], 1 + 0.3*i);
+      }
+    }
+  { // harmonics 20-27
+      float A[] = {0.2, 0.4, 0.6, 1, 0.7, 0.5, 0.3, 0.1};
+      gam::addSines(tb__4, A, 8, 20); // tb__4
+      for (int i = 0; i < 7; i++){
+        addWireBox(mMesh[8], hscaler * A[i], hscaler * A[i+1], 1 + 0.3*i);
+      }
+    }
+
+    // Scale and generate normals
+    for (int i = 0; i < numb_waveform; ++i) {
+      mMesh[i].scale(0.4);
+
+      int Nv = mMesh[i].vertices().size();
+      for (int k = 0; k < Nv; ++k) {
+        mMesh[i].color(HSV(float(k) / Nv, 0.3, 1));
+      }
+
+      if (!vertexLight && mMesh[i].primitive() == Mesh::TRIANGLES) {
+        mMesh[i].decompress();
+      }
+      mMesh[i].generateNormals();
+    }
+  }
+
+  //
+  void onProcess(AudioIOData &io) override
+  {
+    mVib.freq(mVibEnv());
+    float carBaseFreq =
+        getInternalParameterValue("frequency") * getInternalParameterValue("carMul");
+    float modScale = getInternalParameterValue("frequency") * getInternalParameterValue("modMul");
+    float amp = getInternalParameterValue("amplitude") * 0.01;
+    while (io())
+    {
+      mVib.freq(mVibEnv());
+      car.freq((1 + mVib() * mVibDepth) * carBaseFreq +
+               mod() * mModEnv() * modScale);
+      float s1 = car() * mAmpEnv() * amp;
+      // Compute two wet channels of reverberation
+      float wet1, wet2;
+      reverb(s1, wet1, wet2);
+      float filtered, out1, out2;
+      filtered = mFilter(wet1);
+
+      mEnvFollow(filtered);
+      mPan(filtered, out1, out2);
+      io.out(0) += out1;
+      io.out(1) += out2;
+    }
+    if (mAmpEnv.done() && (mEnvFollow.value() < 0.001))
+      free();
+  }
+
+  void onProcess(Graphics &g) override
+  {
+    a += 0.29;
+    b += 0.23;
+    timepose -= 0.6;
+    int shape = getInternalParameterValue("table");
+    g.polygonMode(wireframe ? GL_LINE : GL_FILL);
+    // light.pos(0, 0, 0);
+    gl::depthTesting(true);
+    g.pushMatrix();
+    g.depthTesting(true);
+    g.lighting(true);
+    g.translate(init_pose);
+    // g.translate(getInternalParameterValue("freq") / 100, getInternalParameterValue("freq") / 100 + 2, 0);
+    g.rotate(init_rot_quant, init_rot);
+    g.rotate(mVib(), Vec3f(0, 1, 0));
+    g.rotate(mVib() * mVibDepth, Vec3f(1));
+    float scaling = getInternalParameterValue("amplitude");
+    g.scale(10, 0.1,0.1);
+    g.color(HSV( (getInternalParameterValue("modMul") + init_rot_quant) / 360, 100 * mEnvFollow.value(), mEnvFollow.value()+0.9));
+    g.draw(mMesh[shape]);
+    g.popMatrix();
+  }
+
+  void onTriggerOn() override
+  {
+    timepose = 10;
+    init_pose = Vec3f(al::rnd::uniform(-3., 3.), al::rnd::uniform(-2., 4.), al::rnd::uniform(-5., 5.));
+    init_rot = Vec3f(al::rnd::uniform(-5., 5.), al::rnd::uniform(-2., 5.), al::rnd::uniform(-5., 5.));
+    init_rot_quant = al::rnd::uniform(180., 270.);
+    mAmpEnv.reset();
+    mVibEnv.reset();
+    mModEnv.reset();
+    mVib.phase(0);
+    mod.phase(0);
+    reverb.zero();
+    mFilter.zero();
+    updateFromParameters();
+    updateWaveform();
+
+    float modFreq =
+        getInternalParameterValue("freq") * getInternalParameterValue("modMul");
+    mod.freq(modFreq);
+  }
+  void onTriggerOff() override
+  {
+    mAmpEnv.triggerRelease();
+    mModEnv.triggerRelease();
+    mVibEnv.triggerRelease();
+  }
+
+  void updateFromParameters()
+  {
+    mFilter.type(gam::FilterType(getInternalParameterValue("filterType")));
+    mFilter.freq(getInternalParameterValue("filterFrequency"));
+    mFilter.res(getInternalParameterValue("filterResonance"));
+    mModEnv.levels()[0] = getInternalParameterValue("idx1");
+    mModEnv.levels()[1] = getInternalParameterValue("idx2");
+    mModEnv.levels()[2] = getInternalParameterValue("idx2");
+    mModEnv.levels()[3] = getInternalParameterValue("idx3");
+
+    mAmpEnv.attack(getInternalParameterValue("attackTime"));
+    mAmpEnv.release(getInternalParameterValue("releaseTime"));
+    mAmpEnv.sustain(getInternalParameterValue("sustain"));
+
+    mModEnv.lengths()[0] = getInternalParameterValue("attackTime");
+    mModEnv.lengths()[3] = getInternalParameterValue("releaseTime");
+
+    mVibEnv.levels(getInternalParameterValue("vibRate1"),
+                   getInternalParameterValue("vibRate2"),
+                   getInternalParameterValue("vibRate2"),
+                   getInternalParameterValue("vibRate1"));
+    mVibEnv.lengths()[0] = getInternalParameterValue("vibRise");
+    mVibEnv.lengths()[1] = getInternalParameterValue("vibRise");
+    mVibEnv.lengths()[3] = getInternalParameterValue("vibRise");
+    mVibDepth = getInternalParameterValue("vibDepth");
+    
+    mPan.pos(getInternalParameterValue("pan"));
+    reverb.decay(getInternalParameterValue("reverb"));
+  }
+  void updateWaveform(){
+        // Map table number to table in memory
+    switch (int(getInternalParameterValue("table"))) {
+      case 0:
+        car.source(tbSaw);
+        break;
+      case 1:
+        car.source(tbSqr);
+        break;
+      case 2:
+        car.source(tbImp);
+        break;
+      case 3:
+        car.source(tbSin);
+        break;
+      case 4:
+        car.source(tbPls);
+        break;
+      case 5:
+        car.source(tb__1);
+        break;
+      case 6:
+        car.source(tb__2);
+        break;
+      case 7:
+        car.source(tb__3);
+        break;
+      case 8:
+        car.source(tb__4);
+        break;
+    }
+  }
+};
